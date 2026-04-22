bcff61d881fec473e86a2f70aa65c0e742e07860
JasperLee2027 <jasper.l@wustl.edu>
2026-04-21 21:10:03 -0500

    Update Dormitory.java

--- src/exam/codewriting/q1/Dormitory.java
+++ src/exam/codewriting/q1/Dormitory.java
@@ -17,32 +17,39 @@ public class Dormitory
 	private HashMap < String , Student > rooms ;
 	// Write the constructor for the Dormitory class:
 	// (For full credit the constructor should use only one parameter)
-
+	
+	//CONSTUCTOR ANSWER
+		public Dormitory (String name) {
+			this.name = name
+			this.rooms = new HashMap <Sting, Student> ();
+	//END CONSTUCTOR ANSWER
 
 	//Now, write out the remaining methods:
 	public String getName () {
-		return ""; //FIXME
+		return this.name;
+
+		
 	}
 
 	/**
 	 * Returns the amount of rooms that are currently occupied.
 	 */
 	public int getOccupiedRooms () {
-		return 0;//FIXME
+		return this.rooms.size();
 	}
 	
 	/**
 	* Sets the given room number to be occupied by the given Student.
 	*/
 	public void addStudent ( String roomNo , Student s) {
-		//FIXME
+		this.rooms.put(String roomNo, Student s);
 	}
 	
 	/**
 	* Returns true if the given room contains a Student, false otherwise
 	*/
 	public boolean isOccupied ( String roomNo ) {
-		return false; //FIXME
+		return this.rooms.get(roomNo) != null;
 	}
 	
 	/**
@@ -51,15 +58,22 @@ public class Dormitory
 	* @return A List containing the student ID numbers of all students in this dorm
 	*/
 	public LinkedList < Integer > getStudentIds () {
-		return null; //FIXME
+		LinkedList<Integer> ids = new LinkedList<Integer>(); 
+		for (Map.entry<String, Student> pair : this.rooms.entrySet()){
+			ids.add(pair.getValue().getId());
+		}
+		return ids;
 	}
 	
 	public static void main(String[] args) {
 		
 		//Create an instance of the Dormitory class and store it in a variable called dorm.
-		
 		//Create an instance of Student and add it to the dorm. You can choose which room to add it to.
-		
 		//Write code to determine if a student with the id of 123456 is in your dorm using the appropriate methods.
+
+		Dormitory dorm = new Dormitory ("Beaumont");
+		Student s = new Student(518322);
+		dorm.addStudent("111", s);
+		Sytem.out.println(dorm.getStudentIds().contains(518322));
 	}
 }
