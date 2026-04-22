e1898fd780d051129d9d1d30c5117f225cbe0e69
JasperLee2027 <jasper.l@wustl.edu>
2026-04-21 21:46:38 -0500

    Update SolarSystem.java

--- src/exam/codewriting/q2/SolarSystem.java
+++ src/exam/codewriting/q2/SolarSystem.java
@@ -14,35 +14,46 @@ public class SolarSystem {
 	private LinkedList < Planet > planets ;
 	// Write the constructor for the SolarSystem class.
 	// When a SolarSystem is first created, it should not contain any planets.
-	
-	public String getName () {
-		return ""; //FIXME
-	}
-	
-	public void addPlanet ( Planet p) {
+	public SolarSystem(name){
+		this.name= name
+		this.planets= LinkedList<>();
 		
 	}
 	
+	public String getName () {
+		return name; 
+	}
+	
+	public void addPlanet ( Planet p) {
+		planets.add(p);
+	}
+	
 	public int getNumberOfPlanets () {
-		return 0; //FIXME
+		return planets.size();
 	}
 	
 	/**
 	* Returns the sum of the masses of all planets in the solar system
 	*/
 	public double getTotalMass () {
-		return 0; //FIXME
+		double total= 0;
+		for (Plaent p: planets){
+			total += p.getMass
+		}
+		return total;
 	}
 	
 	public static void main(String[] args) {
 		//Write a line of code that creates an instance of the Planet class by calling the constructor. You should
 		//also declare a variable to hold the newly created instance.
-		
 		//Write a line of code that creates an instance of the SolarSystem class by calling the constructor. You
 		//should also declare a variable to hold the newly created instance.
-	
 		//Write a line of code that will add the Planet to the SolarSystem
-		
 		//Write a line of code that will retrieve the total mass from the SolarSystem
+
+		SolarSystem system = new SolarSystem("Milky Way")
+		Planet earth = new Planet("Earth",100);
+		system.addPlanet(earth);
+		double total = system.getTotalMass();
 	}
 }
